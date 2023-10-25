926e9d44ae181eed72ac76dffffc5915acb140ed
rod773 <104315743+rod773@users.noreply.github.com>
2023-10-25 15:25:37 +0200

    import header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div>
-      <h1>Gsap Rotated Menu</h1>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
